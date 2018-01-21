233046d27b6a40f952312d530972bbdca15f16db
wilythemute <wily@shiriculapo.com>
2018-01-21 12:14:21 -0500

    server-setup

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// Asynchronous
+const crypto = require('crypto').randomBytes(256).toString('hex');
+
+module.exports = {
+	uri: 'mongodb://localhost:27017/' + this.db,
+	secret: crypto,
+	db: 'mean-angular-2'
+}
